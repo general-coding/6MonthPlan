65e122335a4ce3224fef4324b4263960ab42c608
Puneeth Umesh Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2019-07-01 12:30:48 -0500

    Introducing Collections and Arrays : The Array: A Fixed Size Ordered Collection

---
+++ Pluralsight/BegCShCollections/DaysOfWeek.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BegCShCollections
+{
+    class DaysOfWeek
+    {
+        static void Main(string[] args)
+        {
+            string monday = "Monday";
+
+            string[] daysOfWeek = {
+                "Monday"
+                , "Tuesday"
+                , "Wednesday"
+                , "Thursday"
+                , "Friday"
+                , "Saturday"
+                , "Sunday"
+            };
+        }
+    }
+}
