14138e8aa4f8334fd121d02910112069e96cd7b5
Puneeth Umesh Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2019-07-06 18:28:47 -0500

    Raising events

--- Pluralsight/EDEH.DelegatesAndEvents/Worker.cs
+++ Pluralsight/EDEH.DelegatesAndEvents/Worker.cs
@@ -12,7 +12,40 @@ namespace EDEH.DelegatesAndEvents
 
         public void DoWork(int hours, WorkType workType)
         {
+            for (int i = 0; i < hours; i++)
+            {
+                //Raise event work performed
+                OnWorkPerformed(i + 1, workType);
+            }
 
+            //Raise event work completed
+            OnWorkCompleted();
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            //if(WorkPerformed != null)
+            //{
+            //    WorkPerformed(hours, workType);
+            //}
+
+            if (WorkPerformed is WorkPerformedHandler del)
+            {
+                del(hours, workType);
+            }
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            //if(WorkCompleted != null)
+            //{
+            //    WorkCompleted(this, EventArgs.Empty);
+            //}
+
+            if (WorkCompleted is EventHandler del)
+            {
+                del(this, EventArgs.Empty);
+            }
         }
     }
 }
