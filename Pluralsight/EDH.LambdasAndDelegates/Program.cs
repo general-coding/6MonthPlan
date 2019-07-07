a06180265498c6d76b630679a69b0f418ca03a5f
Puneeth Umesh Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2019-07-07 15:34:13 -0500

    Lambdas

---
+++ Pluralsight/EDH.LambdasAndDelegates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDH.LambdasAndDelegates
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var worker = new Worker();
+            worker.WorkPerformed += (s, e) =>
+            {
+                Console.WriteLine("Worked: " + e.Hours + " hour(s) doing: " + e.WorkType);
+                Console.WriteLine("Some other value");
+            };
+            worker.WorkCompleted += (s, e) =>
+            {
+                Console.WriteLine("Work is complete!");
+            };
+            worker.DoWork(8, WorkType.GenerateReports);
+
+            Console.Read();
+        }
+
+        //public static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        //{
+        //    Console.WriteLine("Worked: " + e.Hours + " hour(s) doing: " + e.WorkType);
+        //}
+
+        //public static void Worker_WorkCompleted(object sender, EventArgs e)
+        //{
+        //    Console.WriteLine("Work is complete!");
+        //}
+    }
+}
