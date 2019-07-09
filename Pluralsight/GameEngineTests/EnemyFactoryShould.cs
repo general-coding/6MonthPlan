7091a840a94b3e280f6f312ae9811849a433851f
Puneeth Umesh Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2019-07-09 15:29:14 -0500

    Asserting exceptions and types

---
+++ Pluralsight/GameEngineTests/EnemyFactoryShould.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GameEngine.Tests
+{
+    [TestClass]
+    public class EnemyFactoryShould
+    {
+        [TestMethod]
+        public void NotAllowNullName()
+        {
+            EnemyFactory factory = new EnemyFactory();
+
+            Assert.ThrowsException<ArgumentNullException>
+            (
+                () => factory.Create(null)
+            );
+        }
+
+        [TestMethod]
+        public void OnlyAllowKingOrQueenBossEnemies()
+        {
+            EnemyFactory factory = new EnemyFactory();
+
+            EnemyCreationException ex = Assert.ThrowsException<EnemyCreationException>
+                                        (
+                                            () => factory.Create("Zombie", true)
+                                        );
+
+            Assert.AreEqual("Zombie", ex.RequestedEnemyName);
+        }
+
+        [TestMethod]
+        public void CreateNormalEnemyByDefault()
+        {
+            EnemyFactory factory = new EnemyFactory();
+
+            Enemy enemy = factory.Create("Zombie");
+
+            Assert.IsInstanceOfType(enemy, typeof(NormalEnemy));
+        }
+
+        //[TestMethod]
+        //public void CreateNormalEnemyByDefault_NotTypeExample()
+        //{
+        //    EnemyFactory factory = new EnemyFactory();
+
+        //    Enemy enemy = factory.Create("Zombie");
+
+        //    Assert.IsNotInstanceOfType(enemy, typeof(NormalEnemy));
+        //}
+
+        [TestMethod]
+        public void CreateBossEnemy()
+        {
+            EnemyFactory factory = new EnemyFactory();
+
+            Enemy enemy = factory.Create("Zombie King", true);
+
+            Assert.IsInstanceOfType(enemy, typeof(BossEnemy));
+        }
+
+        [TestMethod]
+        public void CreateSeparateInstances()
+        {
+            EnemyFactory factory = new EnemyFactory();
+
+            Enemy enemy1 = factory.Create("Zombie");
+            Enemy enemy2 = factory.Create("Zombie");
+
+            Assert.AreNotSame(enemy1, enemy2);
+        }
+    }
+}
