eebe7635da079f9c7e3365be1d5549c0ae38c2a0
Puneeth Umesh Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2019-07-09 15:04:09 -0500

    Assert with doubles

---
+++ Pluralsight/GameEngineTests/BossEnemyShould.cs
@@ -0,0 +1,21 @@
+﻿using GameEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameEngineTests
+{
+    [TestClass]
+    public class BossEnemyShould
+    {
+        [TestMethod]
+        public void HaveCorrectSpecialAttackPower()
+        {
+            BossEnemy boss = new BossEnemy();
+
+            //The 0.07 is the delta. It the accuracy required when
+            //performing the assert
+            //This assert will fail, only if the result is more than
+            //the specified delta
+            Assert.AreEqual(166.6, boss.SpecialAttackPower, 0.07);
+        }
+    }
+}
