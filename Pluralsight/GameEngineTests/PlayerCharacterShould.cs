2b9bf2190b215204763f215c91651a106e92a014
Puneeth Umesh Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2019-07-09 14:56:24 -0500

    Assert Numeric results

--- Pluralsight/GameEngineTests/PlayerCharacterShould.cs
+++ Pluralsight/GameEngineTests/PlayerCharacterShould.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using GameEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GameEngine.Test
 {
@@ -19,6 +20,44 @@ namespace GameEngine.Test
             PlayerCharacter player = new PlayerCharacter();
 
             Assert.IsNull(player.Nickname);
+        }
+
+        [TestMethod()]
+        public void StartWithDefaultHealth()
+        {
+            PlayerCharacter player = new PlayerCharacter();
+
+            Assert.AreEqual(100, player.Health);
+        }
+
+        [TestMethod()]
+        public void TakeDamageTest()
+        {
+            PlayerCharacter player = new PlayerCharacter();
+
+            player.TakeDamage(1);
+
+            Assert.AreEqual(99, player.Health);
+        }
+
+        [TestMethod()]
+        public void TakeDamageTest_NotEqual()
+        {
+            PlayerCharacter player = new PlayerCharacter();
+
+            player.TakeDamage(1);
+
+            Assert.AreNotEqual(100, player.Health);
+        }
+
+        [TestMethod()]
+        public void IncreaseHealthAfterSleeping()
+        {
+            PlayerCharacter player = new PlayerCharacter();
+
+            player.Sleep(); //Expect increase between 1 and 100 inclusive
+
+            Assert.IsTrue(player.Health >= 101 && player.Health <= 200);
         }
     }
 }
