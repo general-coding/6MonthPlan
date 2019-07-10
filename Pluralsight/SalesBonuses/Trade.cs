6274d77a840fda540fb292ae1cd9f997ac3db286
Puneeth Umesh Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2019-07-10 18:28:14 -0500

    Set up

---
+++ Pluralsight/SalesBonuses/Trade.cs
@@ -0,0 +1,16 @@
+﻿namespace Pluralsight.ConcurrentCollections.SalesBonuses
+{
+    public class Trade
+    {
+        public SalesPerson Person { get; private set; }
+
+        //QuantitySold is negative if the trade was a purchase
+        public int QuantitySold { get; private set; }
+
+        public Trade(SalesPerson person, int quantitySold)
+        {
+            Person = person;
+            QuantitySold = quantitySold;
+        }
+    }
+}
