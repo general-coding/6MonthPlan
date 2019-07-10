74beeb9ef84cd6457e9862f8aa9c3e245de06bb7
Puneeth Umesh Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2019-07-10 17:58:34 -0500

    Regular queue

---
+++ Pluralsight/ConcurrentQueueDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Pluralsight.ConcurrentCollections.ConcurrentQueueDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //queue
+            DemoQueue();
+        }
+
+        private static void DemoQueue()
+        {
+            Queue<string> shirts = new Queue<string>();
+            shirts.Enqueue("Pluralsight");
+            shirts.Enqueue("WordPress");
+            shirts.Enqueue("Code School");
+
+            Console.WriteLine("After enqueuing, count = " + shirts.Count.ToString());
+
+            string item1 = shirts.Dequeue();
+            Console.WriteLine("\r\nRemoving " + item1);
+
+            string item2 = shirts.Peek();
+            Console.WriteLine("Peeking   " + item2);
+
+            Console.WriteLine("\r\nEnumerating:");
+            foreach (string item in shirts)
+                Console.WriteLine(item);
+
+            Console.WriteLine("\r\nAfter enumerating, count = " + shirts.Count.ToString());
+
+        }
+    }
+
+}
