45688ecdf17c0845495b08e4728f3331be664728
Puneeth Umesh Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2019-07-10 16:54:42 -0500

    Concurrent collections, setup

---
+++ Pluralsight/SubmitOrders/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Pluralsight.ConcurrentCollections.SubmitOrders
+{
+    public class Program
+	{
+        public static void Main(string[] args)
+        {
+            Queue<string> orders = new Queue<string>();
+            PlaceOrders(orders, "Mark");
+            PlaceOrders(orders, "Ramdevi");
+
+            foreach (string order in orders)
+                Console.WriteLine("ORDER: " + order);
+
+            Console.Read();
+        }
+
+        public static void PlaceOrders(Queue<string> orders, string customerName)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				Thread.Sleep(1);
+				string orderName = string.Format("{0} wants t-shirt {1}", customerName, i + 1);
+				orders.Enqueue(orderName);
+			}
+		}
+	}
+
+}
