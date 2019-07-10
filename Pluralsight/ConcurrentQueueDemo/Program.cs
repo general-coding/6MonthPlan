44db110cc8bba833e22b6886fb078ba201549325
Puneeth Umesh Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2019-07-10 17:59:09 -0500

    ConcurrentQueue

--- Pluralsight/ConcurrentQueueDemo/Program.cs
+++ Pluralsight/ConcurrentQueueDemo/Program.cs
@@ -9,7 +9,10 @@ namespace Pluralsight.ConcurrentCollections.ConcurrentQueueDemo
         public static void Main(string[] args)
         {
             //queue
-            DemoQueue();
+            //DemoQueue();
+
+            //oncurrent queue
+            DemoConcurrentQueue();
         }
 
         private static void DemoQueue()
@@ -33,6 +36,36 @@ namespace Pluralsight.ConcurrentCollections.ConcurrentQueueDemo
 
             Console.WriteLine("\r\nAfter enumerating, count = " + shirts.Count.ToString());
 
+        }
+
+        private static void DemoConcurrentQueue()
+        {
+            ConcurrentQueue<string> shirts = new ConcurrentQueue<string>();
+            shirts.Enqueue("Pluralsight");
+            shirts.Enqueue("WordPress");
+            shirts.Enqueue("Code School");
+
+            Console.WriteLine("After enqueuing, count = " + shirts.Count.ToString());
+
+            //= shirts.Dequeue();
+            bool success = shirts.TryDequeue(out string item1);
+            if (success)
+                Console.WriteLine("\r\nRemoving " + item1);
+            else
+                Console.WriteLine("queue was empty");
+
+            //= shirts.Peek();
+            success = shirts.TryPeek(out string item2);
+            if (success)
+                Console.WriteLine("Peeking   " + item2);
+            else
+                Console.WriteLine("queue was empty");
+
+            Console.WriteLine("\r\nEnumerating:");
+            foreach (string item in shirts)
+                Console.WriteLine(item);
+
+            Console.WriteLine("\r\nAfter enumerating, count = " + shirts.Count.ToString());
         }
     }
 
